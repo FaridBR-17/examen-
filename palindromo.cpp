2b961c449856fd1ab99db526bcf7f50d90c8161b
xx-juancamilo777 <juan.gfernandez@mail.escuelaing.edu.co>
2025-09-29 21:44:46 +0000

    commit

---
+++ palindromo.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+bool palindromo(int x) {
+   
+    if (x < 0) return false;
+
+    long long inicial = x;
+    long long invertido = 0;
+
+    while (x > 0) {
+        int digit = x % 10;                                                                                                                                                                
+        invertido= invertido * 10 + digit; 
+        x /= 10;                    
+    }
+
+    return inicial == invertido;
+}
+
+int main() {
+    int x;
+    cout << "Ingrese el numero: ";
+    cin >> x;
+
+    if (palindromo(x))
+        cout << "es palindromo" << endl;
+    else
+        cout << "no es palindromo" << endl;
+
+    return 0;
+}
