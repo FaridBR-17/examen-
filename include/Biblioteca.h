366af9370716d83a9f04e43561ca150909c92196
FaridBR-17 <faridbravo17@gmail.com>
2025-10-20 12:12:19 -0500

    Add Biblioteca class

--- include/Biblioteca.h
+++ include/Biblioteca.h
@@ -1,1 +1,20 @@
+#ifndef BIBLIOTECA_H
+#define BIBLIOTECA_H
 
+#include "Libro.h"
+#include <vector>
+#include <string>
+
+class Biblioteca {
+private:
+    std::vector<Libro> libros;
+
+public:
+    void agregarLibro(const Libro& libro);
+    void eliminarLibro(const std::string& ISBN);
+    void buscarPorTitulo(const std::string& titulo) const;
+    void buscarPorAutor(const std::string& autor) const;
+    void mostrarLibrosDisponibles() const;
+};
+
+#endif
