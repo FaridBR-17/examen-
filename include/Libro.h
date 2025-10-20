ee7f7b848eeea91ce3fe83790c998af18caa8489
FaridBR-17 <faridbravo17@gmail.com>
2025-10-20 12:10:34 -0500

    Add Libro class

--- include/Libro.h
+++ include/Libro.h
@@ -1,1 +1,24 @@
+#ifndef LIBRO_H
+#define LIBRO_H
 
+#include <string>
+
+class Libro {
+private:
+    std::string titulo;
+    std::string autor;
+    std::string ISBN;
+    bool disponible;
+
+public:
+    Libro(std::string tit, std::string aut, std::string isbn);
+    
+    std::string getTitulo() const;
+    std::string getAutor() const;
+    std::string getISBN() const;
+    bool estaDisponible() const;
+    
+    void setDisponible(bool disp);
+};
+
+#endif
