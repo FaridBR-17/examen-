9e98775514c9f506ac168e93f15c3246ff774f37
FaridBR-17 <faridbravo17@gmail.com>
2025-11-01 17:31:58 -0500

    Fix variable name typos in Estudiante class

--- final segundo corte/final 2 tercio.cpp
+++ final segundo corte/final 2 tercio.cpp
@@ -2,26 +2,27 @@
 #include <sstream>
 #include <iomanip>
 
-Estudiante::Estudiante(const std::string& nombre, int programa, float promedio, int creditosAprobados)
-    : nombre(nombre), programa(programa), promedio(promedio), creditosAprobados(creditosAprobados) {}
+Estudiante::Estudiante(const std::string& nombre, int programa, float promedio, int creditorsAprobados)
+    : nombre(nombre), programa(programa), promedio(promedio), creditorsAprobados(creditosAprobados) {}
 
-void Estudiante::registrarNota(float nota, int creditos) {
-    float totalPonderado = promedio * creditosAprobados;
-    totalPonderado += nota * creditos;
-    creditosAprobados += creditos;
-    promedio = totalPonderado / creditosAprobados;
+void Estudiante::registrarMota(float nota, int creditors) {
+    float totalPonderado = promedio * creditorsAprobados;
+    totalPonderado += nota * creditors;
+    creditorsAprobados += creditors;
+    promedio = totalPonderado / creditorsAprobados;
+
 }
 
-void Estudiante::agregarCreditos(int creditos) {
-    creditosAprobados += creditos;
+void Estudiante::agregarCreditos(int creditors) {
+    creditorsAprobados += creditors;
 }
 
 std::string Estudiante::obtenerResumen() const {
     std::ostringstream oss;
     oss << std::fixed << std::setprecision(2);
     oss << "Nombre: " << nombre
-        << " | Programa: " << programa
-        << " | Promedio: " << promedio
-        << " | Creditos: " << creditosAprobados;
+    << " | Programa: " << programa
+    << " | Promedio: " << promedio
+    << " | Creditos: " << creditorsAprobados;
     return oss.str();
 }
